e05b354ae16e8974c54a1dcee22e7aa23e12f755
chaebal <muhdadamyaqin@gmail.com>
2025-05-01 20:13:26 +0800

    redirect to home immediately after loading website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,43 @@
-import React from 'react';
+// import React from 'react';
 
-import SectionBuyOrExplore from './preview-page-sections/BuyOrExplore';
-import SectionHeader from './preview-page-sections/Header';
-import SectionIncludes from './preview-page-sections/Includes';
-import SectionMainPages from './preview-page-sections/MainPages';
-import SectionMid from './preview-page-sections/MidSection';
-import SectionUtilityPages from './preview-page-sections/UtilityPages';
-// import Home from "./home/page";
+// import SectionBuyOrExplore from './preview-page-sections/BuyOrExplore';
+// import SectionHeader from './preview-page-sections/Header';
+// import SectionIncludes from './preview-page-sections/Includes';
+// import SectionMainPages from './preview-page-sections/MainPages';
+// import SectionMid from './preview-page-sections/MidSection';
+// import SectionUtilityPages from './preview-page-sections/UtilityPages';
+import { redirect } from 'next/navigation';
 
 const page = () => {
-  return (
-    <div className="pt-20">
-      <div className="relative pb-24">
-        <SectionHeader />
-      </div>
+  redirect('/home');
+  // return null;
+  // return (
+  //   <div className="pt-20">
+  //     <div className="relative pb-24">
+  //       <SectionHeader />
+  //     </div>
 
-      <div className="bg-gray py-24">
-        <SectionIncludes />
-      </div>
+  //     <div className="bg-gray py-24">
+  //       <SectionIncludes />
+  //     </div>
 
-      <div className="py-24">
-        <SectionMid />
-      </div>
+  //     <div className="py-24">
+  //       <SectionMid />
+  //     </div>
 
-      <div className="bg-gray py-24" id="pages">
-        <SectionMainPages />
-      </div>
+  //     <div className="bg-gray py-24" id="pages">
+  //       <SectionMainPages />
+  //     </div>
 
-      <div className="py-24">
-        <SectionUtilityPages />
-      </div>
+  //     <div className="py-24">
+  //       <SectionUtilityPages />
+  //     </div>
 
-      <div className="mb-24 bg-gray py-24">
-        <SectionBuyOrExplore />
-      </div>
-    </div>
-  );
+  //     <div className="mb-24 bg-gray py-24">
+  //       <SectionBuyOrExplore />
+  //     </div>
+  //   </div>
+  // );
 };
 
 export default page;
