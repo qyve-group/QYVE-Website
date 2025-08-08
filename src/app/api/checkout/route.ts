c9a73c6d410cceb81ccde4058f5f817fe961f444
chaebal <muhdadamyaqin@gmail.com>
2025-08-09 07:20:41 +0800

    v1.0 stripe version

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -16,7 +16,9 @@ import Stripe from 'stripe';
 //   postal_code: string;
 // };
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: '2025-06-30.basil',
+});
 
 export async function POST(req: Request) {
   try {
